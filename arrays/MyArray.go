b679efc053aaf0fbb9b7157685ded78f156448d7
mrpiggy97 <fabyjesusrivas10@gmail.com>
2022-08-28 02:57:57 +0000

    array now take generics, added method to get the index of an element in array, added method that inserts element at a given position in array, added method that returns a slice containing the elements of the array not pointers, added tests for these new methods

--- arrays/MyArray.go
+++ arrays/MyArray.go
@@ -2,7 +2,7 @@ package arrays
 
 import "fmt"
 
-type Array[T any] struct {
+type Array[T comparable] struct {
 	Data   map[int]*T
 	Length *int
 }
@@ -14,8 +14,22 @@ func (array *Array[T]) Append(value *T) {
 
 func (array *Array[T]) PrintMembers() {
 	for index := 0; index < *array.Length; index++ {
-		fmt.Println(array.Data[index])
+		fmt.Println(*array.Data[index])
 	}
+}
+
+func (array *Array[T]) GetIndexOfValue(value T) *int {
+	var correctIndex *int = nil
+	for index := 0; index < *array.Length; index++ {
+		var element T = *array.Get(index)
+		if element == value {
+			fmt.Println("element and index", index, element)
+			correctIndex = &index
+			break
+		}
+	}
+
+	return correctIndex
 }
 
 func (array *Array[T]) Get(index int) *T {
@@ -50,7 +64,43 @@ func (array Array[T]) InsertAtStart(value *T) {
 	}
 }
 
-func NewArray[T any]() *Array[T] {
+func (array *Array[T]) InsertAt(index int, value *T) {
+	if index >= *array.Length {
+		fmt.Println("index out of bounds for value ", *value)
+	} else {
+		var valuesToMove []*T = []*T{}
+		// append all elements that from index foward to valuesToMove
+		for currentIndex := index; currentIndex < *array.Length; currentIndex++ {
+			valuesToMove = append(valuesToMove, array.Get(currentIndex))
+		}
+		// replace item in index with new value
+		array.Data[index] = value
+		// increase length of array
+		*array.Length = *array.Length + 1
+		// index of array in valuesToMove
+		var indexOfElement int = 0
+		// loop through every element after the new element inserted
+		// replace the current value of that element with elements
+		// in valuesToMove slice
+		for currentIndex := index + 1; currentIndex < *array.Length; currentIndex++ {
+			array.Data[currentIndex] = valuesToMove[indexOfElement]
+			indexOfElement = indexOfElement + 1
+		}
+	}
+}
+
+func (array *Array[T]) GetSlice() []T {
+	if *array.Length == 0 {
+		return nil
+	}
+	var values []T = []T{}
+	for index := 0; index < *array.Length; index++ {
+		values = append(values, *array.Get(index))
+	}
+	return values
+}
+
+func NewArray[T comparable]() *Array[T] {
 	var length int = 0
 	return &Array[T]{
 		Data:   make(map[int]*T),
--- arrays/MyArray_test.go
+++ arrays/MyArray_test.go
@@ -1,6 +1,7 @@
 package arrays_test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/mrpiggy97/golang-leetcode/arrays"
@@ -59,7 +60,75 @@ func testInsertAtStart(testCase *testing.T) {
 	}
 }
 
+func testInsertAt(testCase *testing.T) {
+	var testingArray *arrays.Array[int] = arrays.NewArray[int]()
+	var first *int = new(int)
+	var second *int = new(int)
+	var fourth *int = new(int)
+	var fifth *int = new(int)
+	*first = 1
+	*second = 2
+	*fourth = 4
+	*fifth = 5
+
+	var third *int = new(int)
+	*third = 3
+
+	testingArray.Append(first)
+	testingArray.Append(second)
+	testingArray.Append(fourth)
+	testingArray.Append(fifth)
+	testingArray.InsertAt(2, third)
+	var random *int = new(int)
+	*random = 100
+	testingArray.InsertAt(4, random)
+
+	var storedValue int = *testingArray.Get(4)
+
+	if storedValue != *random {
+		testCase.Errorf("expected %d to be %d", storedValue, *third)
+	}
+	var seventh *int = new(int)
+	*seventh = 7
+	testingArray.InsertAt(0, seventh)
+	if *testingArray.Get(0) != 7 {
+		testCase.Errorf("expected %d, got %d instead", *seventh, *testingArray.Get(0))
+	}
+}
+
+func testGetSlice(testCase *testing.T) {
+	var testingArray *arrays.Array[int] = arrays.NewArray[int]()
+	var first *int = new(int)
+	var second *int = new(int)
+	var fourth *int = new(int)
+	var fifth *int = new(int)
+	var third *int = new(int)
+	*first = 1
+	*second = 2
+	*fourth = 4
+	*fifth = 5
+	*third = 100
+	testingArray.Append(first)
+	testingArray.Append(second)
+	testingArray.Append(fourth)
+	testingArray.Append(fifth)
+	testingArray.InsertAt(2, third)
+
+	var expectedResult []int = []int{*first, *second, *third, *fourth, *fifth}
+	var result []int = testingArray.GetSlice()
+
+	for index, value := range result {
+		var currentComparison int = expectedResult[index]
+		if currentComparison != value {
+			testCase.Errorf("%d not equal to %d", value, currentComparison)
+		}
+	}
+	fmt.Println(result)
+}
+
 func TestMyArray(testCase *testing.T) {
 	testCase.Run("action=basic-functionality", testBasicFunctionality)
 	testCase.Run("action=test-insert-at-start", testInsertAtStart)
+	testCase.Run("action=test-insert-at", testInsertAt)
+	testCase.Run("action=test-get-slice", testGetSlice)
 }
