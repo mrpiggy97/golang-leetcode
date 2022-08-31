80fac4eb5c5e5713423f92902e18db6f59a2cb4c
mrpiggy97 <fabyjesusrivas10@gmail.com>
2022-08-31 23:28:06 +0000

    added test case

--- arrays/MaxSubArray_test.go
+++ arrays/MaxSubArray_test.go
@@ -11,6 +11,7 @@ func testMaxSubArray(testCase *testing.T) {
 	var array []int = []int{-2, 1, -3, 4, -1, 2, 1, -5, 4}
 	var secondSlice []int = []int{1}
 	var thirdSlice []int = []int{-1}
+	var fourthSlice []int = []int{-2, -1}
 	var highestSum int = arrays.MaxSubArray(array)
 	var expectedResult int = 6
 	if highestSum != expectedResult {
@@ -24,6 +25,12 @@ func testMaxSubArray(testCase *testing.T) {
 	highestSum = arrays.MaxSubArray(thirdSlice)
 	expectedResult = -1
 
+	if highestSum != expectedResult {
+		testCase.Errorf("expected %d, to be %d", highestSum, expectedResult)
+	}
+
+	highestSum = arrays.MaxSubArray(fourthSlice)
+	expectedResult = -1
 	if highestSum != expectedResult {
 		testCase.Errorf("expected %d, to be %d", highestSum, expectedResult)
 	}
