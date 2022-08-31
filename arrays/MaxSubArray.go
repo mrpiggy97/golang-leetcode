1ae4ae873f199aab54784f25a13ffa25443b4581
mrpiggy97 <fabyjesusrivas10@gmail.com>
2022-08-31 20:25:08 +0000

    created functions that help the highest sum and a sub array in an array, along with tests for it

---
+++ arrays/MaxSubArray.go
@@ -0,0 +1,52 @@
+package arrays
+
+func GetSliceWithHighestSum(slices [][]int) ([]int, int) {
+	var sliceWithHighestValue []int = []int{}
+	var highestSum int = 0
+	for _, slice := range slices {
+		var sum int = 0
+		for _, value := range slice {
+			sum = sum + value
+		}
+		if sum > highestSum {
+			highestSum = sum
+			sliceWithHighestValue = slice
+		}
+	}
+	return sliceWithHighestValue, highestSum
+}
+
+func GetSlicesFromIndex(index int, slice []int) [][]int {
+	var slices [][]int = [][]int{}
+	var endIndex int = len(slice)
+
+	for endIndex != index {
+		var currentSlice []int = []int{}
+		for i := index; i < endIndex; i++ {
+			currentSlice = append(currentSlice, slice[i])
+		}
+		slices = append(slices, currentSlice)
+		endIndex = endIndex - 1
+	}
+
+	return slices
+}
+
+// get the slice with highest sum
+func MaxSubArray(nums []int) int {
+	var slicesWithHighestSum [][]int = [][]int{}
+	if len(nums) == 0 {
+		return 0
+	}
+	if len(nums) == 1 {
+		return nums[0]
+	}
+	for index := range nums {
+		var currentSlice [][]int = GetSlicesFromIndex(index, nums)
+		highestValueSlice, _ := GetSliceWithHighestSum(currentSlice)
+		slicesWithHighestSum = append(slicesWithHighestSum, highestValueSlice)
+	}
+
+	_, sum := GetSliceWithHighestSum(slicesWithHighestSum)
+	return sum
+}
---
+++ arrays/MaxSubArray_test.go
@@ -0,0 +1,57 @@
+package arrays_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/mrpiggy97/golang-leetcode/arrays"
+)
+
+func testMaxSubArray(testCase *testing.T) {
+	var array []int = []int{-2, 1, -3, 4, -1, 2, 1, -5, 4}
+	var secondSlice []int = []int{1}
+	var thirdSlice []int = []int{-1}
+	var highestSum int = arrays.MaxSubArray(array)
+	var expectedResult int = 6
+	if highestSum != expectedResult {
+		testCase.Errorf("expected %d, to be %d", highestSum, expectedResult)
+	}
+	highestSum = arrays.MaxSubArray(secondSlice)
+	expectedResult = 1
+	if highestSum != expectedResult {
+		testCase.Errorf("expected %d, to be %d", highestSum, expectedResult)
+	}
+	highestSum = arrays.MaxSubArray(thirdSlice)
+	expectedResult = -1
+
+	if highestSum != expectedResult {
+		testCase.Errorf("expected %d, to be %d", highestSum, expectedResult)
+	}
+}
+
+func testGetSliceWithHighestSum(testCase *testing.T) {
+	var firstSlice []int = []int{-1, 3, 4, 1}
+	var secondSlice []int = []int{-1, 3, 4}
+	var slices [][]int = [][]int{firstSlice, secondSlice}
+	_, sum := arrays.GetSliceWithHighestSum(slices)
+	var expectedResult int = 7
+	if sum != expectedResult {
+		testCase.Errorf("expected %d to be %d", sum, expectedResult)
+	}
+}
+
+func testGetSlicesFromIndex(testCase *testing.T) {
+	var testingSlice []int = []int{2, -1, 44, 5, 6}
+	var slices [][]int = arrays.GetSlicesFromIndex(1, testingSlice)
+	var length int = 4
+	fmt.Println(slices)
+	if len(slices) != length {
+		testCase.Errorf("length is %d, expected it to be %d", len(slices), length)
+	}
+}
+
+func TestMaxSubArray(testCase *testing.T) {
+	testCase.Run("action=test-max-sub-array", testMaxSubArray)
+	testCase.Run("action=test-get-max-sum", testGetSliceWithHighestSum)
+	testCase.Run("action=test-get-slices-from-index", testGetSlicesFromIndex)
+}
