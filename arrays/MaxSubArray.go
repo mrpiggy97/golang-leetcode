d2f89651671e9ca59007ba4b7fc2f56ab5b86628
mrpiggy97 <fabyjesusrivas10@gmail.com>
2022-09-01 16:22:56 +0000

    simplified algorithm

--- arrays/MaxSubArray.go
+++ arrays/MaxSubArray.go
@@ -1,22 +1,21 @@
 package arrays
 
-func GetHighestSumFromStartingIndex(index int, slice []int) int {
+func GetHighestSumFromStartingIndex(slice []int) int {
 	// this is the lowest value an int can be in golang
 	var highestSum int = -2147483648
-	var endIndex int = len(slice)
-	for endIndex != index {
+	for index := 0; index < len(slice); index++ {
 		var sum int = 0
-		for i := index; i < endIndex; i++ {
-			sum = sum + slice[i]
-			if sum < 0 {
+		for i := index; i < len(slice); i++ {
+			var currentNumber int = slice[i]
+			sum = sum + currentNumber
+			if sum > highestSum {
+				highestSum = sum
+			}
+
+			if currentNumber < 0 && sum < 0 {
 				break
 			}
 		}
-		if sum > highestSum {
-			highestSum = sum
-		}
-
-		endIndex = endIndex - 1
 	}
 	return highestSum
 }
@@ -29,12 +28,6 @@ func MaxSubArray(nums []int) int {
 	if len(nums) == 1 {
 		return nums[0]
 	}
-	var highestSum int = -2147483648
-	for index := range nums {
-		var currentSum int = GetHighestSumFromStartingIndex(index, nums)
-		if currentSum > highestSum {
-			highestSum = currentSum
-		}
-	}
+	var highestSum int = GetHighestSumFromStartingIndex(nums)
 	return highestSum
 }
