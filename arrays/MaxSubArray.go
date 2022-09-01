3bd97636c674a566d65cf7d748125347430f9158
mrpiggy97 <fabyjesusrivas10@gmail.com>
2022-09-01 02:37:05 +0000

    deleted function that return bidimensional array, along with tests for it, GetSliceWithHighestSum now return highest sum of array members begginning in index

--- arrays/MaxSubArray.go
+++ arrays/MaxSubArray.go
@@ -1,53 +1,40 @@
 package arrays
 
-func GetSliceWithHighestSum(slices [][]int) ([]int, int) {
-	var sliceWithHighestValue []int = []int{}
+func GetHighestSumFromStartingIndex(index int, slice []int) int {
 	// this is the lowest value an int can be in golang
 	var highestSum int = -2147483648
-	for _, slice := range slices {
+	var endIndex int = len(slice)
+	for endIndex != index {
 		var sum int = 0
-		for _, value := range slice {
-			sum = sum + value
+		for i := index; i < endIndex; i++ {
+			sum = sum + slice[i]
+			if sum < 0 {
+				break
+			}
 		}
 		if sum > highestSum {
 			highestSum = sum
-			sliceWithHighestValue = slice
 		}
-	}
-	return sliceWithHighestValue, highestSum
-}
 
-func GetSlicesFromIndex(index int, slice []int) [][]int {
-	var slices [][]int = [][]int{}
-	var endIndex int = len(slice)
-
-	for endIndex != index {
-		var currentSlice []int = []int{}
-		for i := index; i < endIndex; i++ {
-			currentSlice = append(currentSlice, slice[i])
-		}
-		slices = append(slices, currentSlice)
 		endIndex = endIndex - 1
 	}
-
-	return slices
+	return highestSum
 }
 
 // get the slice with highest sum
 func MaxSubArray(nums []int) int {
-	var slicesWithHighestSum [][]int = [][]int{}
 	if len(nums) == 0 {
 		return 0
 	}
 	if len(nums) == 1 {
 		return nums[0]
 	}
+	var highestSum int = -2147483648
 	for index := range nums {
-		var currentSlice [][]int = GetSlicesFromIndex(index, nums)
-		highestValueSlice, _ := GetSliceWithHighestSum(currentSlice)
-		slicesWithHighestSum = append(slicesWithHighestSum, highestValueSlice)
+		var currentSum int = GetHighestSumFromStartingIndex(index, nums)
+		if currentSum > highestSum {
+			highestSum = currentSum
+		}
 	}
-
-	_, sum := GetSliceWithHighestSum(slicesWithHighestSum)
-	return sum
+	return highestSum
 }
