113ae8ad5773367739932c01c67160438cf9c483
mrpiggy97 <fabyjesusrivas10@gmail.com>
2022-09-17 20:37:59 +0000

    modified solution and tests

--- linkedLists/MergeLists.go
+++ linkedLists/MergeLists.go
@@ -5,76 +5,69 @@ type ListNode struct {
 	Next *ListNode
 }
 
-func placeValuesOnSplice(node *ListNode, sendValue chan<- []int) {
-	if node != nil {
-		var currentNode *ListNode = node
-		var nodeValues []int = []int{}
-		var currentIndex int = 0
-		for currentNode != nil {
-			if len(nodeValues) == 0 {
-				nodeValues = append(nodeValues, currentNode.Val)
-				currentIndex = currentIndex + 1
-				currentNode = currentNode.Next
-			} else {
-				var nextValue int = currentNode.Val
-				var previousValue int = nodeValues[currentIndex-1]
-				if nextValue < previousValue {
-					nodeValues[currentIndex-1] = nextValue
-					nodeValues = append(nodeValues, previousValue)
-				} else {
-					nodeValues = append(nodeValues, nextValue)
-				}
-				currentIndex = currentIndex + 1
-				currentNode = currentNode.Next
-			}
+func RemoveItemFromSlice(slice []*ListNode, index int) []*ListNode {
+	var newSlice []*ListNode = []*ListNode{}
+	for currentIndex, value := range slice {
+		if currentIndex != index {
+			newSlice = append(newSlice, value)
 		}
-		sendValue <- nodeValues
 	}
-
-	if node == nil {
-		sendValue <- []int{}
-	}
+	return newSlice
 }
 
-func MergeLists(firstNode *ListNode, secondNode *ListNode) *ListNode {
-	var valuesChannel chan []int = make(chan []int, 1)
-	go placeValuesOnSplice(firstNode, valuesChannel)
-	go placeValuesOnSplice(secondNode, valuesChannel)
-	var finalValues []int = <-valuesChannel
-	var valuesOfSecondNode []int = <-valuesChannel
-	finalValues = append(finalValues, valuesOfSecondNode...)
-	if len(finalValues) == 0 {
+func getLowestValue(slice []*ListNode) (lowestValue *ListNode, index int) {
+	index = 0
+	lowestValue = slice[index]
+	for i := index; i < len(slice); i++ {
+		var currentValue *ListNode = slice[i]
+		if currentValue.Val < lowestValue.Val {
+			lowestValue = currentValue
+			index = i
+		}
+	}
+	return lowestValue, index
+}
+
+func SortLowToHigh(nodeSlice []*ListNode) []*ListNode {
+	var sliceCopy []*ListNode = make([]*ListNode, len(nodeSlice))
+	copy(sliceCopy, nodeSlice)
+	var nodesLowToHigh []*ListNode = []*ListNode{}
+	var nodeSliceLength int = len(sliceCopy)
+
+	for nodeSliceLength > 0 {
+		currentValue, currentIndex := getLowestValue(sliceCopy)
+		nodesLowToHigh = append(nodesLowToHigh, currentValue)
+		sliceCopy = RemoveItemFromSlice(sliceCopy, currentIndex)
+		nodeSliceLength = len(sliceCopy)
+	}
+	return nodesLowToHigh
+}
+
+func GetNodes(node *ListNode) []*ListNode {
+	var slice []*ListNode = []*ListNode{}
+	var currentNode *ListNode = node
+	for currentNode != nil {
+		slice = append(slice, currentNode)
+		currentNode = currentNode.Next
+	}
+	return slice
+}
+
+func MergeLists(node1 *ListNode, node2 *ListNode) *ListNode {
+	var nodeSlice []*ListNode = GetNodes(node1)
+	nodeSlice = append(nodeSlice, GetNodes(node2)...)
+	if len(nodeSlice) == 0 {
 		return nil
 	}
-	// sort final array
-	var length int = 1
-	for length != len(finalValues) {
-		for index, value := range finalValues {
-			if index > 0 {
-				var previousValue int = finalValues[index-1]
-				if value < previousValue {
-					finalValues[index-1] = value
-					finalValues[index] = previousValue
-				}
-			}
-		}
-		length = length + 1
-	}
-	var mainNode *ListNode = new(ListNode)
-	var currentNode *ListNode = mainNode
-	for index, value := range finalValues {
+	nodeSlice = SortLowToHigh(nodeSlice)
+	var currentNode *ListNode = nil
+	for index, node := range nodeSlice {
 		if index == 0 {
-			mainNode.Val = value
-			mainNode.Next = nil
-		}
-		if index > 0 {
-			var newNode *ListNode = &ListNode{
-				Val:  value,
-				Next: nil,
-			}
-			currentNode.Next = newNode
-			currentNode = currentNode.Next
+			currentNode = node
+		} else {
+			currentNode.Next = node
+			currentNode = node
 		}
 	}
-	return mainNode
+	return nodeSlice[0]
 }
--- linkedLists/MergeLists_test.go
+++ linkedLists/MergeLists_test.go
@@ -1,13 +1,12 @@
 package linkedlists_test
 
 import (
-	"fmt"
 	"testing"
 
 	linkedlists "github.com/mrpiggy97/golang-leetcode/linkedLists"
 )
 
-func TestMergeLists(testCase *testing.T) {
+func testSortLowToHigh(testCase *testing.T) {
 	var node1 *linkedlists.ListNode = &linkedlists.ListNode{
 		Val:  20,
 		Next: nil,
@@ -30,10 +29,52 @@ func TestMergeLists(testCase *testing.T) {
 	}
 
 	node3.Next = node4
-
-	var finalNode *linkedlists.ListNode = linkedlists.MergeLists(node1, node3)
-	if finalNode.Next.Val != 6 {
-		var actualResult string = fmt.Sprintf("%d", finalNode.Next.Val)
-		testCase.Errorf("finalNode.next.Val is supposed to be 6, got %s instead", actualResult)
+	var nodes []*linkedlists.ListNode = []*linkedlists.ListNode{node1, node2, node3, node4}
+	nodes = linkedlists.SortLowToHigh(nodes)
+	var expectedResult []*linkedlists.ListNode = []*linkedlists.ListNode{node2, node4, node3, node1}
+	for index, node := range nodes {
+		if node.Val != expectedResult[index].Val {
+			testCase.Errorf("expected %d to be %d", node.Val, expectedResult[index].Val)
+		}
 	}
+}
+
+func testMergeLists(testCase *testing.T) {
+	var node1 *linkedlists.ListNode = &linkedlists.ListNode{
+		Val:  20,
+		Next: nil,
+	}
+
+	var node2 *linkedlists.ListNode = &linkedlists.ListNode{
+		Val:  1,
+		Next: nil,
+	}
+	node1.Next = node2
+
+	var node3 *linkedlists.ListNode = &linkedlists.ListNode{
+		Val:  10,
+		Next: nil,
+	}
+
+	var node4 *linkedlists.ListNode = &linkedlists.ListNode{
+		Val:  6,
+		Next: nil,
+	}
+	node3.Next = node4
+
+	var mainNode *linkedlists.ListNode = linkedlists.MergeLists(node1, node3)
+	var expectedResults []int = []int{1, 6, 10, 20}
+	var currentNode *linkedlists.ListNode = mainNode
+
+	for _, value := range expectedResults {
+		if currentNode.Val != value {
+			testCase.Errorf("expected %d to be %d", currentNode.Val, value)
+		}
+		currentNode = currentNode.Next
+	}
+}
+
+func TestMergeLists(testCase *testing.T) {
+	testCase.Run("action=test-sort-low-to-high", testSortLowToHigh)
+	testCase.Run("action=test-merge-lists", testMergeLists)
 }
