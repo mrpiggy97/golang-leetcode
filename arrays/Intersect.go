c466b31853a9ec85c8fbc2d1a10512a8d5ac3e48
mrpiggy97 <fabyjesusrivas10@gmail.com>
2022-10-15 15:54:57 +0000

    added new solution for Intersect problem, modified test to accomodate new solution

--- arrays/Intersect.go
+++ arrays/Intersect.go
@@ -1,77 +1,43 @@
 package arrays
 
-func Contains[T comparable](slice []T, value T) (contains bool, count int) {
-	contains = false
-	count = 0
-	for _, val := range slice {
-		if val == value {
-			if !contains {
-				contains = true
-			}
-			count = count + 1
-		}
-	}
-	return contains, count
-}
-
-func CountElement[T comparable](slice []T, value T) (count int) {
-	count = 0
-	for _, val := range slice {
-		if val == value {
-			count = count + 1
-		}
-	}
-	return count
-}
-
-func GetElementsInCommon[T comparable](slice1 []T, slice2 []T) []T {
-	var elements []T = []T{}
-	if len(slice1) < len(slice2) {
-		for _, value := range slice1 {
-			valueInSlice2, _ := Contains[T](slice2, value)
-			if valueInSlice2 {
-				valueAlreadyInElements, _ := Contains[T](elements, value)
-				if !valueAlreadyInElements {
-					elements = append(elements, value)
-				}
-			}
-		}
-	} else {
-		for _, value := range slice2 {
-			valueInSlice1, _ := Contains[T](slice1, value)
-			if valueInSlice1 {
-				valueAlreadyInElements, _ := Contains[T](elements, value)
-				if !valueAlreadyInElements {
-					elements = append(elements, value)
-				}
-			}
-		}
-	}
-	return elements
-}
-
-func Intersect(nums1 []int, num2 []int) []int {
+func Intersect(nums1, nums2 []int) []int {
+	var nums1Count map[int]int = make(map[int]int)
+	var nums2Count map[int]int = make(map[int]int)
 	var intersection []int = []int{}
-	var elementsInCommon []int = GetElementsInCommon[int](nums1, num2)
-	for _, value := range elementsInCommon {
-		var countInNums1 int = CountElement[int](nums1, value)
-		var countInNum2 int = CountElement[int](num2, value)
-		var countUsed int = 0
+	for _, val := range nums1 {
+		count, exists := nums1Count[val]
+		if !exists {
+			nums1Count[val] = 1
+		} else {
+			nums1Count[val] = count + 1
+		}
+	}
 
-		if countInNums1 != countInNum2 {
-			if countInNums1 < countInNum2 {
-				countUsed = countInNums1
-			} else {
-				countUsed = countInNum2
+	for _, val := range nums2 {
+		count, exists := nums2Count[val]
+		if !exists {
+			nums2Count[val] = 1
+		} else {
+			nums2Count[val] = count + 1
+		}
+	}
+
+	for val, count := range nums1Count {
+		countInNums2, exists := nums2Count[val]
+		if exists {
+			var finalCount int = -1
+			if count > countInNums2 {
+				finalCount = countInNums2
 			}
-		}
-		if countInNums1 == countInNum2 {
-			// you can either use countInNums1 or countInNum2
-			// if they are of the same value
-			countUsed = countInNums1
-		}
-		for i := 0; i < countUsed; i++ {
-			intersection = append(intersection, value)
+			if countInNums2 > count {
+				finalCount = count
+			}
+			if count == countInNums2 {
+				finalCount = count
+			}
+			for i := 0; i < finalCount; i++ {
+				intersection = append(intersection, val)
+			}
 		}
 	}
 	return intersection
--- arrays/Intersect_test.go
+++ arrays/Intersect_test.go
@@ -1,41 +1,41 @@
 package arrays_test
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/mrpiggy97/golang-leetcode/arrays"
 )
 
-func testGetElementsInCommon(testCase *testing.T) {
-	var testSlice1 []int = []int{222, 1, 6, 7, 10}
-	var testSlice2 []int = []int{4, 6, 6, 6, 91, 100, 222, 1}
-	var result []int = arrays.GetElementsInCommon[int](testSlice1, testSlice2)
-	var expectedResult []int = []int{222, 1, 6}
-	for index, value := range result {
-		if value != expectedResult[index] {
-			testCase.Errorf("expected %d to be %d", value, expectedResult[index])
-		}
-	}
-}
-
 func testIntersection(testCase *testing.T) {
 	var nums1 []int = []int{1, 8, 7, 9, 2, 9, 100, 9, 9, 9}
 	var nums2 []int = []int{1, 9, 33, 5, 190, 9, 9, 177, 12, 122}
-	var expectedResult = []int{1, 9, 9, 9}
 	var result []int = arrays.Intersect(nums1, nums2)
-	if len(result) != len(expectedResult) {
+	var expectedResult map[int]int = make(map[int]int)
+	expectedResult[1] = 1
+	expectedResult[9] = 3
+	if len(result) != 4 {
 		testCase.Errorf("length of slices is not the same")
 	}
-	for index, value := range expectedResult {
-		if value != result[index] {
-			testCase.Errorf("expected %d to be %d", value, expectedResult[index])
+	for _, val := range result {
+		count, exists := expectedResult[val]
+		if !exists {
+			testCase.Errorf("expected %d to exist in expected results", val)
+		}
+		if exists {
+			if val == 9 {
+				if count != 3 {
+					testCase.Errorf("expected count for %d to be %d, got %d instead", val, 3, count)
+				}
+			}
+			if val == 1 {
+				if count != 1 {
+					testCase.Errorf("expected count for %d to be %d, got %d instead", val, 1, count)
+				}
+			}
 		}
 	}
-	fmt.Println(result)
 }
 
 func TestIntersection(testCase *testing.T) {
-	testCase.Run("action=test-get-elements-in-common", testGetElementsInCommon)
 	testCase.Run("action=test-intersection", testIntersection)
 }
