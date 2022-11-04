a509ebcd95504f73656e293f11c2ea9d9233c458
mrpiggy97 <fabyjesusrivas10@gmail.com>
2022-11-04 18:23:11 +0000

    this solution beats runtime

--- stringMethods/LongestPalindrome.go
+++ stringMethods/LongestPalindrome.go
@@ -1,58 +1,39 @@
 package stringMethods
 
-import (
-	"strings"
-)
-
-func IsPalindrome(subStr []string) bool {
-	var endIndex int = int(len(subStr) / 2)
-	var currentTail int = len(subStr) - 1
-	for index := range subStr[0:endIndex] {
-		if subStr[index] != subStr[currentTail] {
+func IsPalindrome(str string, startingIndex, tailIndex int) bool {
+	var subStr string = str[startingIndex:tailIndex] + string(str[tailIndex])
+	var splitIndex int = int(len(subStr) / 2)
+	var currentEnd int = len(subStr) - 1
+	for i := 0; i < splitIndex; i++ {
+		var head byte = subStr[i]
+		var tail byte = subStr[currentEnd]
+		if head != tail {
 			return false
 		}
-		currentTail = currentTail - 1
+		currentEnd = currentEnd - 1
 	}
 	return true
 }
 
-func GetClosureIndexes(index int, subStr []string) []int {
-	var closuresIndexes []int = []int{}
-	for i, val := range subStr {
-		if i != index {
-			if val == subStr[index] {
-				closuresIndexes = append(closuresIndexes, i)
-			}
-		}
-	}
-	return closuresIndexes
-}
-
-func GetSubStr(str []string, startingIndex int, endIndex int) []string {
-	var newStr []string = []string{}
-	for i := startingIndex; i <= endIndex; i++ {
-		newStr = append(newStr, str[i])
-	}
-	return newStr
-}
-
 func LongestPalindrome(str string) string {
-	var strSlice []string = strings.Split(str, "")
-	var selectedSlice []string = []string{string(str[0])}
-	for index := range strSlice {
-		var closureIndexes []int = GetClosureIndexes(index, strSlice)
-		if len(closureIndexes) > 0 {
-			// we start with the largest possible palindrome
-			for currentIndex := len(closureIndexes) - 1; currentIndex >= 0; currentIndex-- {
-				var subSlice []string = GetSubStr(strSlice, index, closureIndexes[currentIndex])
-				var subSliceIsPalindrome bool = IsPalindrome(subSlice)
-				if subSliceIsPalindrome {
-					if len(subSlice) > len(selectedSlice) {
-						selectedSlice = subSlice
+	if len(str) == 0 {
+		return str
+	}
+	var selectedPalindrome string = string(str[0])
+	for index, value := range str {
+		for tail := len(str) - 1; tail > index; tail-- {
+			var tailValue string = string(str[tail])
+			if tailValue == string(value) {
+				var isPalindrome bool = IsPalindrome(str, index, tail)
+				if isPalindrome {
+					var newStr = string(str[index:tail])
+					newStr = newStr + tailValue
+					if len(newStr) > len(selectedPalindrome) {
+						selectedPalindrome = newStr
 					}
 				}
 			}
 		}
 	}
-	return strings.Join(selectedSlice, "")
+	return selectedPalindrome
 }
--- stringMethods/LongestPalindrome_test.go
+++ stringMethods/LongestPalindrome_test.go
@@ -1,9 +1,6 @@
 package stringMethods_test
 
 import (
-	"fmt"
-	"os"
-	"strings"
 	"testing"
 
 	"github.com/mrpiggy97/golang-leetcode/stringMethods"
@@ -11,52 +8,32 @@ import (
 
 func testIsPalindrome(testCase *testing.T) {
 	var testingStr string = "aba"
-	var result bool = stringMethods.IsPalindrome(strings.Split(testingStr, ""))
+	var result bool = stringMethods.IsPalindrome(testingStr, 0, len(testingStr)-1)
 	var expectedResult bool = true
 	if result != expectedResult {
 		testCase.Errorf("expected %v to be %v", result, expectedResult)
 	}
 
 	testingStr = "aas43"
-	result = stringMethods.IsPalindrome(strings.Split(testingStr, ""))
+	result = stringMethods.IsPalindrome(testingStr, 3, len(testingStr)-1)
 	expectedResult = false
 	if result != expectedResult {
 		testCase.Errorf("expected %v to be %v", result, expectedResult)
 	}
 
 	testingStr = "anitalavalatina"
-	result = stringMethods.IsPalindrome(strings.Split(testingStr, ""))
+	result = stringMethods.IsPalindrome(testingStr, 0, len(testingStr)-1)
 	expectedResult = true
 	if result != expectedResult {
 		testCase.Errorf("expected %v to be %v", result, expectedResult)
 	}
-}
 
-func testGetClosureIndexes(testCase *testing.T) {
-	var testingStr string = "fabi1011f"
-	var testingSlice []string = strings.Split(testingStr, "")
-	var result []int = stringMethods.GetClosureIndexes(0, strings.Split(testingStr, ""))
-	if len(result) != 1 {
-		testCase.Errorf("expected length of result to be 1, got %d, instead", len(result))
-		os.Exit(2)
-	}
-	if result[0] != len(testingSlice)-1 {
-		testCase.Errorf("expected index to be %d got %d instead", len(testingSlice)-1, result[0])
-	}
-
-	result = stringMethods.GetClosureIndexes(2, testingSlice)
-	if len(result) != 0 {
-		testCase.Errorf("expected length of result to be 1, got %d, instead", len(result))
-		os.Exit(2)
-	}
-}
-
-func testGetSubString(testCase *testing.T) {
-	var testingString string = "testingsubstr"
-	var result []string = stringMethods.GetSubStr(strings.Split(testingString, ""), 1, 4)
-	if len(result) != 4 {
-		fmt.Println(result)
-		testCase.Errorf("expected length of result to be 4, got %d instead", len(result))
+	testingStr = "abafafaaaaaf"
+	result = stringMethods.IsPalindrome(testingStr, 5, 11)
+	//fmt.Println(string(testingStr[5:11] + string(testingStr[11])))
+	expectedResult = true
+	if result != expectedResult {
+		testCase.Errorf("expected %v to be %v", result, expectedResult)
 	}
 }
 
@@ -85,7 +62,5 @@ func testLongestPalindrome(testCase *testing.T) {
 
 func TestLongestPalindrome(testCase *testing.T) {
 	testCase.Run("action=test-is-palindrome", testIsPalindrome)
-	testCase.Run("action=test-get-closure-indexes", testGetClosureIndexes)
-	testCase.Run("action=test-get-sub-string", testGetSubString)
 	testCase.Run("action=test-longest-palindrome", testLongestPalindrome)
 }
